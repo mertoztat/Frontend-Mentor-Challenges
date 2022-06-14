696d786e91a663e8c52d4dfd11d717a003a50abc
Mert Öztat <mertoztat@gmail.com>
2022-06-14 22:31:13 +0300

    localstorage added

--- Landing-page-dark-light-theme/script.js
+++ Landing-page-dark-light-theme/script.js
@@ -1,6 +1,27 @@
 const body = document.querySelector("body");
 const check = document.getElementById("check");
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const currentTheme = localStorage.getItem("theme");
 
-check.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
-});
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    document.body.classList.toggle("dark");
+  }
+}
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    document.body.classList.toggle("dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+    document.body.classList.toggle("dark");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
