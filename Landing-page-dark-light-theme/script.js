edcf3b6efb10cac17130ccf51ac9753afa1ff885
Mert Öztat <mertoztat@gmail.com>
2022-06-17 15:14:58 +0300

    darktheme, localstorage & hamburger menu added

--- Landing-page-dark-light-theme/script.js
+++ Landing-page-dark-light-theme/script.js
@@ -1,6 +1,38 @@
 const body = document.querySelector("body");
 const check = document.getElementById("check");
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const currentTheme = localStorage.getItem("theme");
 
-check.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
+// dark & light theme switch
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    document.body.classList.toggle("dark");
+  }
+}
+
+// set & get localstorage dark-light theme
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    document.body.classList.toggle("dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+    document.body.classList.toggle("dark");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+//hamburger menu
+const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
